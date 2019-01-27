cb35217374dafae82c927d72ed2cd1fce53d89bb
Anderson Molter <39280342+andersonmolter1@users.noreply.github.com>
2019-01-27 15:53:50 -0500

    Added Body Mechanics and Basic Sprite Character
    Respawn and jumping mechanics have been implemented.

--- mutiplayer2DFightingTAP/mutiplayer2DFightingTAP/Assets/Scripts/PLayerController.cs
+++ mutiplayer2DFightingTAP/mutiplayer2DFightingTAP/Assets/Scripts/PLayerController.cs
@@ -6,40 +6,80 @@ using UnityEngine.Networking;
 public class PLayerController : NetworkBehaviour
 {
     public float speed;
-   
+
     private Rigidbody rb;
 
-
+    public int jumpForce;
+    private bool isGrounded = false;
     public Vector3 pos;
 
     // Use this for initializati
     void Start()
     {
+        facingRight = true;
         rb = GetComponent<Rigidbody>();
     }
 
     void Update()
     {
+        jumpMovement();
+        horizontalMovement();
+    }
+    void horizontalMovement()
+    {
         if (isLocalPlayer)
-        {
-                float moveHorizontal = Input.GetAxisRaw("Horizontal");
-                Vector2 movement = new Vector2(moveHorizontal, 0.0f);
-      
-            rb.constraints = RigidbodyConstraints.None;
-            if (Input.GetKeyUp("a") ||Input.GetKeyUp("d"))
             {
-                for (int i = 5; i > 0; i--)
-                {
-                    rb.AddForce(((movement * speed) / i) * -1);
-                }
-                rb.constraints = RigidbodyConstraints.FreezeAll;
-            } 
+
+            float moveHorizontal = Input.GetAxisRaw("Horizontal");
+            Vector2 movement = new Vector2(moveHorizontal, 0.0f);
+
+            if (Input.GetKeyUp("a") || Input.GetKeyUp("d"))
+            {
+                rb.constraints = RigidbodyConstraints.FreezePositionX;
+                rb.constraints = RigidbodyConstraints.None;
+                rb.constraints = RigidbodyConstraints.FreezeRotationX | RigidbodyConstraints.FreezeRotationZ | RigidbodyConstraints.FreezeRotationY;
+            }
             else
             {
                 rb.AddForce(movement * speed);
             }
-            Debug.Log("Key down");
-            return;
+            
         }
     }
+    void jumpMovement()
+    {
+        if (isLocalPlayer)
+        {
+            if (Input.GetKeyDown("space") && isGrounded == true)
+            {
+                rb.AddForce(new Vector2(0, jumpForce), ForceMode.Impulse);
+            }
+            else
+            {
+
+            }
+
+        }
+    }
+    void OnCollisionEnter(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Platform")
+        {
+            isGrounded = true;
+        }
+    }
+
+    //consider when character is jumping .. it will exit collision.
+    void OnCollisionExit(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "Platform")
+        {
+            isGrounded = false;
+        }
+    }
+    void RotateLeft()
+    {
+        rb.transform.Rotate(Vector3.forward * -90);
+    }
+    
 }
---
+++ mutiplayer2DFightingTAP/mutiplayer2DFightingTAP/Assets/Scripts/Respawn.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Networking;
+
+public class Respawn : NetworkBehaviour
+{
+    
+    private bool isDead = false;
+    public GameObject player;
+    public Vector3 spawnPosition1;
+    public Vector3 spawnPosition2;
+
+
+    void Update()
+    {
+        
+
+        if (isLocalPlayer)
+        {
+            if (isDead)
+            {
+                bool randomBool = (Random.value > 0.5f);
+                
+
+                if (randomBool)
+                {
+                    player.transform.position = (spawnPosition1);
+
+                }
+                else if (!randomBool)
+                {
+                   
+                    player.transform.position = (spawnPosition2);
+                }
+
+            }
+            
+        }
+    }
+    void OnCollisionEnter(Collision theCollision)
+    {
+        if (theCollision.gameObject.tag == "bottomFloor")
+        {
+            isDead = true;  
+        }
+    }
+    void OnCollisionExit(Collision theCollision)
+    {
+        if (theCollision.gameObject.name == "bottomFloor")
+        {
+            isDead = false;
+        }
+    }
+
+    IEnumerator WaitTime()
+    {
+        yield return new WaitForSecondsRealtime(2);
+    }
+}
