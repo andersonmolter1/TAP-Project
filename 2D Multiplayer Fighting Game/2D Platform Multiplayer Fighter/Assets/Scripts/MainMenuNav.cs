d7b8f48425ae5bb17eadae58c9d29ea86412b3a4
Anderson Molter <andersonmolter1@gmail.com>
2019-02-20 19:43:17 -0500

    New Features
    Added:
    -4 player mode with controllers
    -basic menu with button navigation
    -improved player control and body
    mechanics
    -stock game from unity as core example
    -respawning player (work in progress)
    -mapping of player input
    -basic map for example

---
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/MainMenuNav.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class MainMenuNav : MonoBehaviour
+{
+    public Button Button;
+    public int levelIndex = 0;
+    void Start()
+    {
+        Button.onClick.AddListener(delegate { LoadByIndex(levelIndex); });
+    }
+
+
+    public void LoadByIndex(int levelIndex)
+    {
+        SceneManager.LoadScene(1);
+     }
+}
---
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/Respawner.cs
@@ -0,0 +1,49 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class Respawner : MonoBehaviour
+{
+	public GameObject splash;
+    public GameObject spawnPoint;
+    public GameObject player;
+    public string playerTag = "player1";
+    private string killBar = "Respawner";
+    public int spawnX = 0;
+    public int spawnY = 0;
+    public int spawnZ = 0;
+
+
+
+    void OnCollisionEnter2D(Collider2D col)
+	{
+        // If the player hits the trigger...
+        if (col.gameObject.CompareTag("Respawn"))
+        {
+            // .. stop the camera tracking the player
+
+
+            // .. stop the Health Bar following the player
+            if (GameObject.FindGameObjectWithTag("HealthBar").activeSelf)
+            {
+                GameObject.FindGameObjectWithTag("HealthBar").SetActive(false);
+            }
+
+            Debug.Log("Player Contact");
+            // ... destroy the player.
+            Destroy(player);
+            // ... reload the level.
+            WaitToLoad();
+            Vector2 vec = new Vector2(spawnX, spawnY);
+            player.transform.position = vec;
+
+        }
+	}
+
+	IEnumerator WaitToLoad()
+	{
+        // ... pause briefly
+        yield return new WaitForSeconds(2);
+       
+	}
+}
