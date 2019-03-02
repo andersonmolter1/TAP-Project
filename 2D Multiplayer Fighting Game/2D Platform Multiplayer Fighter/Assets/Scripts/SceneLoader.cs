ea5ac0fadb7b8d59cf9e261413aca9fd1fb7edd6
ekim22 <ekim2@ggc.edu>
2019-03-02 16:38:22 -0500

    Big changes to Pirate level. Huge changes to level nav. Now purely operated through "SceneLoader.cs" and nothing else. Pirate ship now also navigates to character selection menu and char selection works as intended. Spawn points and respawn have also been integrated into PirateShip.
    The functionality almost perfectly mirrors Castle level. This isn't a statement about how the level should be implemented, and is more about consistency's sake.

--- 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/SceneLoader.cs
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/SceneLoader.cs
@@ -4,13 +4,22 @@ using UnityEngine.SceneManagement;
 /*
  * 
  * Need to fix/separate character selection from levels.
- */ 
- 
+ */
+
 public class SceneLoader : MonoBehaviour
 {
-   
-    // Start is called before the first frame update
-    public void StartLevel(string level)
+    public static string level;
+
+    public void SetLevel(string thisLevel)
+    {
+        level = thisLevel;
+    }
+
+    public void CharacterSelect() {
+        SceneManager.LoadScene("CharacterSelection");
+    }
+
+    public void StartLevel()
     {
         SceneManager.LoadScene(level);
     }
