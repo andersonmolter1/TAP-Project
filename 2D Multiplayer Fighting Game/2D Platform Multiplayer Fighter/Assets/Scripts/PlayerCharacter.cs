193f6b3bb343377cb34bb0eee8d4d4eb9a46d298
ekim22 <ekim2@ggc.edu>
2019-03-10 15:50:12 -0400

    Character assignment to player

---
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/PlayerCharacter.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerCharacter : MonoBehaviour
+{
+    public GameObject player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void setCharacter(GameObject character)
+    {
+        player = character;
+    }
+}
