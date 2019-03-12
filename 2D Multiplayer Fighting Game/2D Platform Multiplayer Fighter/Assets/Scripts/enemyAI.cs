d9d333e2bfe07b46382f4aa2db45952d85233547
Anderson Molter <39280342+andersonmolter1@users.noreply.github.com>
2019-03-11 23:31:35 -0400

    added a enemyAI to follow player

---
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/enemyAI.cs
@@ -0,0 +1,34 @@
+﻿                 // How many times the enemy can be hit before it dies.
+using UnityEngine;
+using System.Collections;
+
+public class enemyAI : MonoBehaviour
+{
+    private float speed = 10.0f;
+    private Vector2 target;
+    private Vector2 position;
+    private Camera cam;
+    private findPlayer playerLoc;
+
+    void Start()
+    {
+        position = gameObject.transform.position;
+        target = playerLoc.location;
+        Debug.Log(target);
+
+    }
+
+    void Update()
+    {
+        target = playerLoc.location;
+        Debug.Log(target);
+        float step = speed * Time.deltaTime;
+
+        // move sprite towards the target location
+        transform.position = Vector2.MoveTowards(transform.position, target, step);
+    }
+
+    
+
+
+}
