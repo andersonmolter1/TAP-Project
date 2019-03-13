b97a7356dda341a5da8c4914e899a73dfe71ac3c
ekim22 <ekim2@ggc.edu>
2019-03-13 11:12:56 -0400

    Missing something with the character spawns. Created player prefabs with the markers. May revert to old spawn system.

---
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/SpawnPlayers.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SpawnPlayers : MonoBehaviour
+{
+    public GameObject p1_spawn, p2_spawn, p3_spawn, p4_spawn;
+    public PlayerCharacter Player1, Player2, Player3, Player4;
+
+    void Start()
+    {
+        spawn();
+    }
+
+    public void spawn()
+    {
+        Player1.getCharacter().transform.position = p1_spawn.transform.position;
+        Player2.getCharacter().transform.position = p2_spawn.transform.position;
+        Player3.getCharacter().transform.position = p3_spawn.transform.position;
+        Player4.getCharacter().transform.position = p4_spawn.transform.position;
+    }
+
+}
