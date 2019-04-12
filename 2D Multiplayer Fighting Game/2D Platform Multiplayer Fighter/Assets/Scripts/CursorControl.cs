7aace13b6c548b1e3e358d54b1752e9cc64ec04e
ekim22 <ekim2@ggc.edu>
2019-04-12 16:48:41 -0400

    Cursor disabled and individual pause system

---
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/CursorControl.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CursorControl : MonoBehaviour
+{
+    public bool isDisabled;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (isDisabled)
+        {
+            Disable();
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown("tab"))
+        {
+            Enable();
+        }
+        if (Input.GetKeyDown("space"))
+        {
+            Disable();
+        }
+    }
+
+    private void Disable()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Enable()
+    {
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+}
--- 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/Pauser.cs
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/Pauser.cs
@@ -1,11 +1,13 @@
 ﻿using UnityEngine;
 using System.Collections;
 using UnityEngine.SceneManagement;
+using UnityEngine.EventSystems;
 
-public class Pauser : MonoBehaviour {
-	private bool paused = false;
+public class Pauser : MonoBehaviour
+{
+    private bool paused = false;
     GameObject[] pauseObjects;
-
+    StandaloneInputModule sim;
 
     private void Start()
     {
@@ -14,16 +16,36 @@ public class Pauser : MonoBehaviour {
     }
 
     // Update is called once per frame
-    void Update () {
-		if(Input.GetKeyDown(KeyCode.Escape) || Input.GetKeyDown(KeyCode.JoystickButton7))
-		{
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
             togglePause();
-		}
+        }
+        if (Input.GetButtonDown("P1_Start"))
+        {
+            togglePause();
+            setSIM("P1");
+        }
+        if (Input.GetButtonDown("P2_Start"))
+        {
+            togglePause();
+            setSIM("P2");
+        }
+        if (Input.GetButtonDown("P3_Start"))
+        {
+            togglePause();
+            setSIM("P3");
+        }
+        if (Input.GetButtonDown("P4_Start"))
+        {
+            togglePause();
+            setSIM("P4");
+        }
+    }
 
-
-	}
-
-    public void togglePause() {
+    public void togglePause()
+    {
         if (paused)
         {
             Time.timeScale = 1f;
@@ -35,22 +57,59 @@ public class Pauser : MonoBehaviour {
             Time.timeScale = 0f;
             paused = true;
             showPaused();
+
+
         }
     }
 
 
     public void showPaused()
     {
-        foreach(GameObject g in pauseObjects)
+        foreach (GameObject g in pauseObjects)
         {
             g.SetActive(true);
         }
     }
 
-    public void hidePaused() {
-        foreach(GameObject g in pauseObjects)
+    public void hidePaused()
+    {
+        foreach (GameObject g in pauseObjects)
         {
             g.SetActive(false);
+        }
+    }
+
+    void setSIM(string player)
+    {
+        if (player.Equals("P1"))
+        {
+            sim.horizontalAxis = "P1_Dpad_Horizontal";
+            sim.verticalAxis = "P1_Dpad_Vertical";
+            sim.submitButton = "P1_Submit";
+            sim.cancelButton = "P1_Cancel";
+        }
+        if (player.Equals("P2"))
+        {
+
+            sim.horizontalAxis = "P2_Dpad_Horizontal";
+            sim.verticalAxis = "P2_Dpad_Vertical";
+            sim.submitButton = "P2_Submit";
+            sim.cancelButton = "P2_Cancel";
+        }
+        if (player.Equals("P3"))
+        {
+
+            sim.horizontalAxis = "P3_Dpad_Horizontal";
+            sim.verticalAxis = "P3_Dpad_Vertical";
+            sim.submitButton = "P3_Submit";
+            sim.cancelButton = "P3_Cancel";
+        }
+        if (player.Equals("P4"))
+        {
+            sim.horizontalAxis = "P4_Dpad_Horizontal";
+            sim.verticalAxis = "P4_Dpad_Vertical";
+            sim.submitButton = "P4_Submit";
+            sim.cancelButton = "P4_Cancel";
         }
     }
 }
