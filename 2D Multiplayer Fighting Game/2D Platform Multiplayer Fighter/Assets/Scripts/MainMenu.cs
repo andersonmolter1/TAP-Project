04da16b4fabb81c721c32f2897d52cd403b96095
ekim22 <ekim2@ggc.edu>
2019-04-12 17:01:25 -0400

    Ported MainMenu script

---
+++ 2D Multiplayer Fighting Game/2D Platform Multiplayer Fighter/Assets/Scripts/MainMenu.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("P1_Submit")
+            || Input.GetKeyDown(KeyCode.Return)
+            || Input.GetKeyDown("space")
+            || Input.GetMouseButton(0))
+        {
+            SceneManager.LoadScene("MapSelector");
+        }
+    }
+}
